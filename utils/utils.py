628404bf91d1ea360c8e0f8d75921bc7648eca80
DemoAuguste <393493772@qq.com>
2020-02-26 23:42:40 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -5,6 +5,16 @@ from keras.datasets import cifar10, cifar100, mnist
 import keras
 from datetime import datetime
 
+def color_preprocessing(x_train, x_test):
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    mean = [125.307, 122.95, 113.865]
+    std = [62.9932, 62.0887, 66.7048]
+    for i in range(3):
+        x_train[:, :, :, i] = (x_train[:, :, :, i] - mean[i]) / std[i]
+        x_test[:, :, :, i] = (x_test[:, :, :, i] - mean[i]) / std[i]
+    return x_train, x_test
+
 def load_dataset(dataset='cifar10', preprocessing=True, shuffle=True):
     """
     return: x_train, x_test, y_train, y_test
