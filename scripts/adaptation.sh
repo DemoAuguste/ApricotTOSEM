f06d9074eb49febb61bf1d357154c5037c9c0bed
DemoAuguste <393493772@qq.com>
2020-03-10 14:24:24 +0800

    Update adaptation.sh

--- scripts/adaptation.sh
+++ scripts/adaptation.sh
@@ -11,6 +11,7 @@ adaptation_path=$working_dir"/"$script_name
 echo $train_path
 
 for i in {2..5}
+do
     CUDA_VISIDBLE_DEVICES=1 python $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
     # CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
     # CUDA_VISIDBLE_DEVICES=0 python $train_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
@@ -18,6 +19,7 @@ for i in {2..5}
 done
 
 for i in {1..3}
+do
     CUDA_VISIDBLE_DEVICES=1 python $train_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
     CUDA_VISIDBLE_DEVICES=1 python $train_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
     # CUDA_VISIDBLE_DEVICES=1 python $train_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
