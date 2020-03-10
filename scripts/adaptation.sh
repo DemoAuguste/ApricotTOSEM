5510b07995baacb448c2c99c664850ea88a9e8c3
DemoAuguste <393493772@qq.com>
2020-03-10 14:22:35 +0800

    update

--- scripts/adaptation.sh
+++ scripts/adaptation.sh
@@ -11,8 +11,14 @@ adaptation_path=$working_dir"/"$script_name
 echo $train_path
 
 for i in {1..5}
-    CUDA_VISIDBLE_DEVICES=0 python $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
-    CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
-    CUDA_VISIDBLE_DEVICES=0 python $train_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
-    CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIDBLE_DEVICES=1 python $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
+    # CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
+    # CUDA_VISIDBLE_DEVICES=0 python $train_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
+    # CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
+done
+
+for i in {1..3}
+    CUDA_VISIDBLE_DEVICES=1 python $train_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIDBLE_DEVICES=1 python $train_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIDBLE_DEVICES=1 python $train_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
 done
