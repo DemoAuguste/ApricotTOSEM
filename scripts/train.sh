e34718ee32ef1304f4bf30cf1a95128941945837
DemoAuguste <393493772@qq.com>
2020-03-11 10:29:34 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -11,14 +11,16 @@ train_path=$working_dir"/"$script_name
 echo $train_path
 
 # script for training models
-for i in {2..5}
-do
-    CUDA_VISIDBLE_DEVICES=0 python $train_path -m resnet20 -d cifar100 -v $i
-done
+# for i in {2..5}
+# do
+#     CUDA_VISIDBLE_DEVICES=0 python $train_path -m resnet20 -d cifar100 -v $i
+# done
 
 for i in {1..5}
 do
-    CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenet -d cifar100 -v $i
-    CUDA_VISIDBLE_DEVICES=0 python $train_path -m resnet32 -d cifar100 -v $i
+    # CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenet -d cifar100 -v $i
+    # CUDA_VISIDBLE_DEVICES=0 python $train_path -m resnet32 -d cifar100 -v $i
     CUDA_VISIDBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar100 -v $i
+    CUDA_VISIDBLE_DEVICES=0 python $train_path -m densenet -d cifar100 -v $i
 done
+
