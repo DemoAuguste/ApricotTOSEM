ccb74a327b682f9a46c4a179d73244e8c6b3fe46
DemoAuguste <393493772@qq.com>
2020-03-18 11:21:23 +0800

    Update adaptation.sh

--- scripts/adaptation.sh
+++ scripts/adaptation.sh
@@ -10,18 +10,23 @@ adaptation_path=$working_dir"/"$script_name
 
 echo $adaptation_path
 
-# for i in {2..5}
-# do
-#     CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
-#     # CUDA_VISIBLE_DEVICES=0 python $adaptation_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
-#     # CUDA_VISIBLE_DEVICES=0 python $adaptation_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
-#     # CUDA_VISIBLE_DEVICES=0 python $adaptation_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
-# done
 
+# CIFAR-100 
 for i in {1..5}
 do
+    # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
     # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
     # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
     CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
     CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m densenet -d cifar100 -v $i -s 1 -a binary
 done
+
+# CIFAR-10
+# for i in {1..5}
+# do
+    # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m resnet20 -d cifar10 -v $i -s 1 -a binary
+    # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m mobilenet -d cifar10 -v $i -s 1 -a binary
+    # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m resnet32 -d cifar10 -v $i -s 1 -a binary
+    # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m mobilenetv2 -d cifar10 -v $i -s 1 -a binary
+    # CUDA_VISIBLE_DEVICES=1 python $adaptation_path -m densenet -d cifar10 -v $i -s 1 -a binary
+# done
