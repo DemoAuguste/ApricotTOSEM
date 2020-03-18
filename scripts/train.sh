43eeeed0c33ef9c8486870401be99331057aa9a5
DemoAuguste <393493772@qq.com>
2020-03-18 09:48:34 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -11,16 +11,24 @@ train_path=$working_dir"/"$script_name
 echo $train_path
 
 # script for training models
-# for i in {2..5}
-# do
-#     CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet20 -d cifar100 -v $i
-# done
 
-for i in {1..5}
-do
+# CIFAR-100
+# for i in {1..5}
+# do
+    # CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet20 -d cifar100 -v $i
     # CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenet -d cifar100 -v $i
     # CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet32 -d cifar100 -v $i
-    CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar100 -v $i
-    CUDA_VISIBLE_DEVICES=0 python $train_path -m densenet -d cifar100 -v $i
+    # CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar100 -v $i
+    # CUDA_VISIBLE_DEVICES=0 python $train_path -m densenet -d cifar100 -v $i
+# done
+
+# CIFAR-10 
+for i in {1..5}
+do
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet20 -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenet -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet32 -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m densenet -d cifar10 -v $i
 done
 
