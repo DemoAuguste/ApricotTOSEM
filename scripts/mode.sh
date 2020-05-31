5d970c7a4c8bfdff82a317ccf1cfee30e5aaa772
DemoAuguste <393493772@qq.com>
2020-05-31 10:36:49 +0800

    Create mode.sh

---
+++ scripts/mode.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# initialization
+working_dir=$(dirname "$PWD") 
+script_name="mode.py"
+# echo $current_path
+# echo $working_dir
+
+train_path=$working_dir"/"$script_name
+
+echo $train_path
+
+# script for training models
+
+# CIFAR-100
+for i in {1..5}
+do
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet20 -d cifar100 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenet -d cifar100 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet32 -d cifar100 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar100 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m densenet -d cifar100 -v $i
+done
+
+# CIFAR-10 
+for i in {1..5}
+do
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet20 -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenet -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m resnet32 -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m mobilenetv2 -d cifar10 -v $i
+    CUDA_VISIBLE_DEVICES=0 python $train_path -m densenet -d cifar10 -v $i
+done
