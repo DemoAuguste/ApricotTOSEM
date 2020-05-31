dc64da275bfd1c8ae1526bcf96320c2d8a34a6ad
DemoAuguste <393493772@qq.com>
2020-05-31 12:46:42 +0800

    Update layer_selection.py

--- MODE/layer_selection.py
+++ MODE/layer_selection.py
@@ -24,7 +24,10 @@ def Forward_Layer_Select(model,
 
     for layer_num, layer in zip(np.arange(1, len(model.layers)), model.layers): 
         try:
-            if isinstance(layer, k.layers.Conv2D):
+            # if isinstance(layer, k.layers.Conv2D):
+            #     continue
+
+            if layer_num != 70:
                 continue
 
             if verbose:
