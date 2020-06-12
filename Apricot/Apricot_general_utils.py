92b8a413f31653f99662727aab6e728168ef2d17
DemoAuguste <393493772@qq.com>
2020-06-12 15:56:55 +0800

    Update Apricot_general_utils.py

--- Apricot/Apricot_general_utils.py
+++ Apricot/Apricot_general_utils.py
@@ -1,6 +1,9 @@
 from sklearn.metrics import classification_report
+import numpy as np
 
 def get_class_acc(model, xs, ys):
     y_pred = model.predict(xs)
+    print(y_pred.shape)
+    y_pred = np.argmax(y_pred, axis=1)
     print(classification_report(ys, y_pred))
     
