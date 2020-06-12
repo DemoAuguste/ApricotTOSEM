48fb3d34be49bef5a383c47c97157ef99053a15b
DemoAuguste <393493772@qq.com>
2020-06-12 15:52:23 +0800

    Update Apricot_general_utils.py

--- Apricot/Apricot_general_utils.py
+++ Apricot/Apricot_general_utils.py
@@ -2,5 +2,5 @@ from sklearn.metrics import classification_report
 
 def get_class_acc(model, xs, ys):
     y_pred = model.predict(xs)
-    print(classification_report(Y_test, y_pred))
+    print(classification_report(ys, y_pred))
     
