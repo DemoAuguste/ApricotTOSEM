3eecc0e3e16dc804141fd7a3c88fa8a8f4a17908
DemoAuguste <393493772@qq.com>
2020-06-12 15:39:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ from Apricot import *
 
 if __name__ == '__main__':
     input_size = (32, 32, 3)
-    model = build_networks('resnet20', num_classes=10, input_size=input_size) 
+    model = build_networks('resnet20', num_classes=100, input_size=input_size) 
     # model.summary()
     # model = build_networks('densenet', num_classes=100, input_size=input_size)
     # model.summary()
     
 
-    x_train, x_test, y_train, y_test = load_dataset('cifar10')
+    x_train, x_test, y_train, y_test = load_dataset('cifar100')
 
     model.fit(x_train, y_train, batch_size=128, epochs=5)
     
