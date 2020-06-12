b9226e568a00489bcb57dd42dc8ebfbf1f54f51b
DemoAuguste <393493772@qq.com>
2020-06-12 16:04:26 +0800

    Update Apricot_general_utils.py

--- Apricot/Apricot_general_utils.py
+++ Apricot/Apricot_general_utils.py
@@ -6,5 +6,6 @@ def get_class_acc(model, xs, ys):
     print(y_pred.shape)
     y_pred = np.argmax(y_pred, axis=1)
     print(y_pred.shape)
+    print(y_pred[0])
     print(classification_report(ys, y_pred))
     
