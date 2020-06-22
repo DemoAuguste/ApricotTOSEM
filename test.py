8fb03471ee9c9331b4d7af4ede74ddf871e4cc30
DemoAuguste <393493772@qq.com>
2020-06-22 10:15:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     # calculate the predicted probability of each class
     pred_prob_mat = model.predict(x_train)
-    max_ind_mat = list(map(lambda x: x==max(x), x)) * np.ones(shape=x.shape)
+    max_ind_mat = list(map(lambda x: x==max(x), pred_prob_mat)) * np.ones(shape=pred_prob_mat.shape)
     max_prob_mat = pred_prob_mat * max_ind_mat
     sum_prob_mat = np.sum(max_prob_mat, axis=0)
     class_prob_mat = sum_prob_mat / len(x_train)
