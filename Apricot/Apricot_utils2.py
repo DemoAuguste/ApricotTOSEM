50f07c2a472778902f6b80f475427bfd16c804ef
DemoAuguste <393493772@qq.com>
2020-06-22 16:34:19 +0800

    Update Apricot_utils2.py

--- Apricot/Apricot_utils2.py
+++ Apricot/Apricot_utils2.py
@@ -35,7 +35,7 @@ def get_sum_sign_weights(sign_weights_list):
         if sum_sign_weights is None:
             sum_weights = w
         else:
-            sum_sign_weights = [item[0] + item[1] for item in zip(sum_weights, w)]
+            sum_weights = [item[0] + item[1] for item in zip(sum_weights, w)]
     return sum_weights
 
 
