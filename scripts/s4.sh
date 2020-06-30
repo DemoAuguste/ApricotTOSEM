f3598a1f5e28f2e049fc516653de38f048642fc0
DemoAuguste <393493772@qq.com>
2020-06-30 09:56:47 +0800

    Create s4.sh

---
+++ scripts/s4.sh
@@ -0,0 +1,28 @@
+working_dir=$(dirname "$PWD") 
+script_name="main_adaptation.py"
+# echo $current_path
+# echo $working_dir
+
+adaptation_path=$working_dir"/"$script_name
+
+echo $adaptation_path
+
+
+for i in {1..5}
+do
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet20 -d cifar100 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenet -d cifar100 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet32 -d cifar100 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenetv2 -d cifar100 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m densenet -d cifar100 -v $i -s 4 -a binary
+done
+
+
+for i in {1..5}
+do
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet20 -d cifar10 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenet -d cifar10 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet32 -d cifar10 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenetv2 -d cifar10 -v $i -s 4 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m densenet -d cifar10 -v $i -s 4 -a binary
+done
