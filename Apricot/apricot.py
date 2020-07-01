c0c3bbbeb2f57d6b696c05e5b363bb3d0117cf6b
DemoAuguste <393493772@qq.com>
2020-07-01 12:47:11 +0800

    Update apricot.py

--- Apricot/apricot.py
+++ Apricot/apricot.py
@@ -334,7 +334,7 @@ def apricot2(model, model_weights_dir, dataset, adjustment_strategy, activation=
     fail_xs, fail_ys, fail_ys_label, fail_num = get_failing_cases(fixed_model, x_train_val, y_train_val)
 
     if settings.NUM_SUBMODELS == 20:
-        sub_correct_matrix_path = os.path.join(model_weights_dir, 'corr_matrix_{}.npy'.format(settings.RANDOM_SEED))
+        sub_correct_matrix_path = os.path.join(model_weights_dir, 'corr_matrix_{}_{}.npy'.format(settings.RANDOM_SEED, settings.NUM_SUBMODELS))
     else:
         sub_correct_matrix_path = os.path.join(model_weights_dir, 'corr_matrix_{}_{}.npy'.format(settings.RANDOM_SEED, settings.NUM_SUBMODELS))
     sub_correct_matrix = None # 1: predicts correctly, 0: predicts incorrectly.
