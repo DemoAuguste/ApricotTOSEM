77e0a9dc0c40cc0b22b46113bed7e51bdb88e7a9
DemoAuguste <393493772@qq.com>
2020-07-01 13:09:00 +0800

    Update adaptation.sh

--- scripts/adaptation.sh
+++ scripts/adaptation.sh
@@ -14,11 +14,11 @@ echo $adaptation_path
 # CIFAR-100 
 for i in {2..5}
 do
-    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
-    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
-    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
-    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
-    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m densenet -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
+    CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m densenet -d cifar100 -v $i -s 1 -a binary
 done
 
 # for i in {1..5}
