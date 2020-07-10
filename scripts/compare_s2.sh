e63a375049ef78c4dea562b5f3c6b4109697e284
DemoAuguste <393493772@qq.com>
2020-07-10 22:20:16 +0800

    update

---
+++ scripts/compare_s2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# initialization
+working_dir=$(dirname "$PWD") 
+script_name="main_adaptation3.py"
+# echo $current_path
+# echo $working_dir
+
+adaptation_path=$working_dir"/"$script_name
+
+echo $adaptation_path
+
+
+# CIFAR-100 
+# for i in {2..5}
+# do
+#     CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet20 -d cifar100 -v $i -s 1 -a binary
+#     CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenet -d cifar100 -v $i -s 1 -a binary
+#     CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m resnet32 -d cifar100 -v $i -s 1 -a binary
+#     CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m mobilenetv2 -d cifar100 -v $i -s 1 -a binary
+#     CUDA_VISIBLE_DEVICES=3 python3 $adaptation_path -m densenet -d cifar100 -v $i -s 1 -a binary
+# done
+
+for i in {1..5}
+do
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m resnet20 -d cifar100 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m mobilenet -d cifar100 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m resnet32 -d cifar100 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m mobilenetv2 -d cifar100 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m densenet -d cifar100 -v $i -s 2 -a binary
+done
---
+++ scripts/compare_s22.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# initialization
+working_dir=$(dirname "$PWD") 
+script_name="main_adaptation3.py"
+# echo $current_path
+# echo $working_dir
+
+adaptation_path=$working_dir"/"$script_name
+
+echo $adaptation_path
+
+
+
+for i in {1..5}
+do
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m resnet20 -d cifar10 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m mobilenet -d cifar10 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m resnet32 -d cifar10 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m mobilenetv2 -d cifar10 -v $i -s 2 -a binary
+    CUDA_VISIBLE_DEVICES=2 python3 $adaptation_path -m densenet -d cifar10 -v $i -s 2 -a binary
+done
