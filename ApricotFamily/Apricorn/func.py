724beaceeef95d84b54878a201a1001c45f0d210
DemoAuguste <393493772@qq.com>
2020-12-07 14:23:37 +0800

    update

---
+++ ApricotFamily/Apricorn/func.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def apricorn_batch_adjust_w(curr_w, batch_corr_mat, weights_list, lr=0.01):
+    """
+    same as apricot lite.
+    """
+    adjust_w = curr_w
+    # region apricot lite adjustment process
+    for i in range(batch_corr_mat.shape[0]):
+        temp_corr_mat = batch_corr_mat[i]
+        temp_incorr_mat = np.ones(temp_corr_mat.shape) - temp_corr_mat
+        # randomly choose one.
+        corr_w = adjust_w
+        incorr_w = adjust_w
+        if len(np.nonzero(temp_corr_mat)[0]) != 0:
+            corr_idx = np.nonzero(temp_corr_mat)[0]
+            # print(corr_idx)
+            corr_idx = corr_idx[np.random.randint(corr_idx.shape[0], size=1)]
+            # print(corr_idx)
+            corr_w = weights_list[corr_idx[0]]
+        if len(np.nonzero(temp_incorr_mat)[0]) != 0:
+            incorr_idx = np.nonzero(temp_incorr_mat)[0]
+            # print(incorr_idx)
+            # print(incorr_idx.shape)
+            incorr_idx = incorr_idx[np.random.randint(incorr_idx.shape[0], size=1)]
+            incorr_w = weights_list[incorr_idx[0]]
+
+        diff_corr_w = [lr * (item[0] - item[1]) for item in zip(adjust_w, corr_w)]
+        diff_incorr_w = [lr * (item[0] - item[1]) for item in zip(adjust_w, incorr_w)]
+
+        adjust_w = [item[0] - item[1] + item[2] for item in zip(curr_w, diff_corr_w, diff_incorr_w)]
+    # endregion
+
+    return adjust_w
+
+
+def apricorn_update_weights_list(curr_w, batch_corr_mat, weights_list, lr=0.01):
+    for i in range(batch_corr_mat.shape[0]):
+        temp_corr_mat = batch_corr_mat[i]
+        temp_incorr_mat = np.ones(temp_corr_mat.shape) - temp_corr_mat
+
+        # try: updates incorrect weights.
+        if len(np.nonzero(temp_incorr_mat)[0]) != 0:
+            # updates weights list
+            incorr_idx = np.nonzero(temp_incorr_mat)[0]
+            for idx in incorr_idx:
+                temp_diff_w = [lr * (item[0] - item[1]) for item in zip(weights_list[idx], curr_w)]
+                weights_list[idx] = [item[0] - item[1] for item in zip( weights_list[idx], temp_diff_w)]
+        else:
+            continue
+
+    return weights_list
---
+++ ApricotFamily/Apricorn/main.py
@@ -0,0 +1,116 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+from utils import load_dataset, split_validation_dataset
+from utils import logger
+from settings import NUM_SUBMODELS, LOOP_COUNT, FIX_BATCH_SIZE
+from .func import *
+from settings import BATCH_SIZE, FURTHER_ADJUSTMENT_EPOCHS
+import os
+import numpy as np
+from ApricotFamily.ApricotPlus.func import batch_get_adjust_w
+from ApricotFamily.Apricot.func import get_indexed_failing_cases, apricot_cal_sub_corr_mat, get_weights_list
+from datetime import datetime
+
+def apricorn(model, model_weights_dir, dataset):
+    """
+    the basic idea of apricorn is to update rDLMs at the same time.
+    """
+    x_train, x_test, y_train, y_test = load_dataset(dataset)
+    x_train_val, x_val, y_train_val, y_val = split_validation_dataset(x_train, y_train)
+
+    train_size = len(x_train)
+    val_size = len(x_train_val)
+    test_size = len(x_test)
+
+    fixed_model = model
+    submodel_dir = os.path.join(model_weights_dir, 'submodels')
+    trained_weights_path = os.path.join(model_weights_dir, 'trained.h5')
+    fixed_weights_path = os.path.join(model_weights_dir, 'apricorn_fixed.h5')
+    log_path = os.path.join(model_weights_dir, 'apricorn.log')
+
+    datagen = ImageDataGenerator(horizontal_flip=True,
+                                 width_shift_range=0.125,
+                                 height_shift_range=0.125,
+                                 fill_mode='constant', cval=0.)
+    datagen.fit(x_train)
+
+    fixed_model.load_weights(trained_weights_path)
+    start = datetime.now()
+    logger('---------------original model---------------', log_path)
+    # region initialization
+    _, base_train_acc = fixed_model.evaluate(x_train_val, y_train_val)
+    _, base_val_acc = fixed_model.evaluate(x_val, y_val)
+    _, base_test_acc = fixed_model.evaluate(x_test, y_test)
+
+    logger('train acc: {:.4f}, val acc: {:.4f}, test acc: {:.4f}'.format(base_train_acc, base_val_acc, base_test_acc),
+           log_path)
+
+    fail_xs, fail_ys, fail_ys_label, fail_num, fail_index = get_indexed_failing_cases(fixed_model, x_train, y_train)
+    print('getting sub correct matrix...')
+    sub_correct_matrix_path = os.path.join(model_weights_dir, 'corr_mat_{}.npy'.format(NUM_SUBMODELS))
+
+    if not os.path.exists(sub_correct_matrix_path):
+        print('generating matrix....')
+        sub_correct_mat = apricot_cal_sub_corr_mat(fixed_model, submodel_dir, fail_xs, fail_ys,
+                                                   num_submodels=NUM_SUBMODELS)
+        np.save(sub_correct_matrix_path, sub_correct_mat)
+    else:
+        print('loading matrix...')
+        sub_correct_mat = np.load(sub_correct_matrix_path)
+
+    fixed_model.load_weights(trained_weights_path)
+
+    weights_list = get_weights_list(fixed_model, submodel_dir, NUM_SUBMODELS)
+
+    best_train_acc = base_train_acc
+    best_val_acc = base_val_acc
+    best_test_acc = base_test_acc
+
+    # if not os.path.exists(fixed_weights_path):
+    fixed_model.save_weights(fixed_weights_path)
+    # endregion
+
+    # Apricorn: iterates all failing cases.
+    for count in range(3):
+        np.random.shuffle(sub_correct_mat)
+        # for i in range()
+        iter_count, res = divmod(sub_correct_mat.shape[0], FIX_BATCH_SIZE)
+        if res != 0:
+            iter_count += 1
+        for i in range(iter_count):
+            curr_w = fixed_model.get_weights()
+            batch_corr_mat = sub_correct_mat[FIX_BATCH_SIZE * i: FIX_BATCH_SIZE * (i + 1)]
+            adjust_w = apricorn_batch_adjust_w(curr_w, batch_corr_mat, weights_list)  # update in lite way.
+
+            fixed_model.set_weights(adjust_w)
+
+            x = int(count * sub_correct_mat.shape[0] + i + 1)
+            y = int(LOOP_COUNT * sub_correct_mat.shape[0])
+            _, curr_acc = fixed_model.evaluate(x_val, y_val)
+            print('[iteration {}/{}] current val acc: {:.4f}'.format(x, y, curr_acc))
+
+            if curr_acc > best_val_acc:
+                best_val_acc = curr_acc
+                fixed_model.save_weights(fixed_weights_path)
+                logger('Improved. val acc: {:.4f}'.format(best_val_acc), log_path)
+
+                # train the fixed model.
+                checkpoint = ModelCheckpoint(fixed_weights_path, monitor='val_accuracy', verbose=1, save_best_only=True,
+                                             mode='max')
+                checkpoint.best = best_val_acc
+                hist = fixed_model.fit_generator(datagen.flow(x_train_val, y_train_val, batch_size=BATCH_SIZE),
+                                                 steps_per_epoch=len(x_train_val) // BATCH_SIZE + 1,
+                                                 validation_data=(x_val, y_val),
+                                                 epochs=3,  # 3 epochs
+                                                 callbacks=[checkpoint])
+                fixed_model.load_weights(fixed_weights_path)
+
+                temp_val_acc = np.max(np.array(hist.history['val_accuracy']))
+                if temp_val_acc > best_val_acc:
+                    # val acc improved.
+                    best_val_acc = temp_val_acc
+                # Apricorn: update weights list.
+                weights_list = apricorn_update_weights_list(curr_w, batch_corr_mat, weights_list)  # lr=0.01
+            else:
+                fixed_model.load_weights(fixed_weights_path)
+
