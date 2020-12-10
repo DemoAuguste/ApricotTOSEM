99e173e325bee886201966a6aa3423f81096cbc6
DemoAuguste <393493772@qq.com>
2020-12-10 21:11:35 +0800

    Update main.py

--- ApricotFamily/Apricot/main.py
+++ ApricotFamily/Apricot/main.py
@@ -115,7 +115,7 @@ def apricot(model, model_weights_dir, dataset, adjustment_strategy):
             print('After adjustment, the val acc: {:.4f}, best val acc: {:.4f}'.format(curr_acc, best_val_acc))
 
             if curr_acc > best_val_acc:
-                best_val_acc = curr_accS
+                best_val_acc = curr_acc
                 fixed_model.save_weights(fixed_weights_path)
                 # further training process.
                 checkpoint = ModelCheckpoint(fixed_weights_path, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
