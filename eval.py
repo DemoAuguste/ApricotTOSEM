07c1be0011883774fe137b0d753596887ac3760a
DemoAuguste <393493772@qq.com>
2021-05-14 15:01:16 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -52,27 +52,27 @@ if __name__ == '__main__':
     x_train_val, x_val, y_train_val, y_val = split_validation_dataset(x_train, y_train)
 
     # evaluate submodels
-    avg_train_acc = []
-    avg_val_acc = []
-    avg_test_acc = []
-    for i in range(20):
-        print('[sub {}] '.format(i))
-        temp_path = os.path.join(sub_dir, 'sub_{}.h5'.format(i))
-        model.load_weights(temp_path)
-        ret = model.evaluate(x_train_val, y_train_val)
-        avg_train_acc.append(float(ret[1]))
-        ret = model.evaluate(x_val, y_val)
-        avg_val_acc.append(float(ret[1]))
-        ret = model.evaluate(x_test, y_test)
-        avg_test_acc.append(float(ret[1]))
+    # avg_train_acc = []
+    # avg_val_acc = []
+    # avg_test_acc = []
+    # for i in range(20):
+    #     print('[sub {}] '.format(i))
+    #     temp_path = os.path.join(sub_dir, 'sub_{}.h5'.format(i))
+    #     model.load_weights(temp_path)
+    #     ret = model.evaluate(x_train_val, y_train_val)
+    #     avg_train_acc.append(float(ret[1]))
+    #     ret = model.evaluate(x_val, y_val)
+    #     avg_val_acc.append(float(ret[1]))
+    #     ret = model.evaluate(x_test, y_test)
+    #     avg_test_acc.append(float(ret[1]))
 
-    # mean and std
-    avg_train_acc = np.array(avg_train_acc).flatten()
-    avg_val_acc = np.array(avg_val_acc).flatten()
-    avg_test_acc = np.array(avg_test_acc).flatten()
-    cal_mean_and_std(avg_train_acc)
-    cal_mean_and_std(avg_val_acc)
-    cal_mean_and_std(avg_test_acc)
+    # # mean and std
+    # avg_train_acc = np.array(avg_train_acc).flatten()
+    # avg_val_acc = np.array(avg_val_acc).flatten()
+    # avg_test_acc = np.array(avg_test_acc).flatten()
+    # cal_mean_and_std(avg_train_acc)
+    # cal_mean_and_std(avg_val_acc)
+    # cal_mean_and_std(avg_test_acc)
 
     # apricorn
     print('------ fixed by apricorn -----')
